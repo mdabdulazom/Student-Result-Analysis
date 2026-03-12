eb0257a307e261c2f1298514c22846877b211ed9
mdabdulazom <mdabdulazom@gmail.com>
2026-03-12 11:47:13 +0600

    Create print many sentence in single line.cpp

---
+++ print many sentence in single line.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello World " <<  "from Apna College " ;
+  return 0;
+}
+
+________
+#include<iostream>
+using namespace std;
+int main(){
+  cout << "Hello \n Mohammad Aftab Hossain";
+return 0;
+}
