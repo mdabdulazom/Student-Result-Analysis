a1eb5b3c1fdc7cee828feb48d02ea237935054e0
mdabdulazom <mdabdulazom@gmail.com>
2026-03-13 10:07:30 +0600

    Create determine data type size.cpp

---
+++ determine data type size.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ int main(){
+     int age = 25;
+     char grade = 'A';
+     float A = 25.00;
+     double B = 30.00;
+     
+     cout << sizeof(age) << "\n" << sizeof(grade) << "\n" << sizeof(A) << "\n" << sizeof(B);
+     
+ }
